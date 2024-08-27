691a8173809be69f3eb5d607d270d286015a0d40
mask-shakill <shakil.cse201@gmail.com>
2024-08-28 00:08:05 +0600

    added shakil-script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Home = () => {
-  return <div>hello shakil </div>;
+  return <div>hello shakil 
+    <h1>added shakil-script</h1>
+  </div>;
 };
 
 export default Home;
